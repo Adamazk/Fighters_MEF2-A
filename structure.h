9b355f1c0d490c2ff99fb2418100b2ba5509f4df
Adamazk <adamarezkii@gmail.com>
2025-05-06 19:59:09 +0200

    Update structure.h

--- structure.h
+++ structure.h
@@ -1,13 +1,14 @@
-#ifndef STRUCTURES_H
-#define STRUCTURES_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h> // Pour la fonction sleep
 #include <limits.h> // Pour INT_MAX
 
-#define MAX_TEAM_NAME 50
+#define MAX_TEAM_NAME 50  // Add this line
+
+void clear_terminal() {
+    printf("\033[H\033[J"); // Séquence ANSI pour nettoyer le terminal
+}
 
 typedef struct {
     char nom[50];
@@ -37,7 +38,7 @@ typedef struct {
     int pv;
 } equipe;
 
-int pv_equipe(equipe equipe1);
-personnage* vitesse_equipe_membre(equipe *equipe);
-
+int pv_equipe(equipe equipe1) {
+    return equipe1.membre1.pv + equipe1.membre2.pv + equipe1.membre3.pv;
+}
 #endif
