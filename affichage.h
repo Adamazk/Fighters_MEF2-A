2f9d82c40af59dbb4d34ee075483c0fa2999075b
Adamazk <adamarezkii@gmail.com>
2025-05-06 12:00:36 +0200

    Create affichage.h

---
+++ affichage.h
@@ -0,0 +1,14 @@
+#ifndef AFFICHAGE_H
+#define AFFICHAGE_H
+
+#include "structures.h"
+
+void clear_terminal();
+void afficher_animation_attaque(const char *attaquant, const char *cible);
+void afficher_animation_speciale(const char *attaquant, const char *cible, const char *type);
+void afficher_menu_mode_de_jeu();
+void afficher_personnages_disponibles(personnage *personnages, int taille);
+void afficher_introduction();
+void afficher_menu_principal();
+
+#endif
