317949db53dc4d73782b31a9722f88e6ecec834f
Adamazk <adamarezkii@gmail.com>
2025-05-08 22:52:00 +0200

    Update equipe.c

--- equipe.c
+++ equipe.c
@@ -8,12 +8,15 @@
 void creer_equipe(equipe *equipe, personnage *personnages, int taille, int mode) {
     clear_terminal();
     printf("Entrez le nom de votre équipe : ");
-    while (getchar() != '\n'); // Vider le buffer avant de lire le nom
-    fgets(equipe->nom, MAX_TEAM_NAME, stdin);
-    equipe->nom[strcspn(equipe->nom, "\n")] = 0;
+    while (getchar() != '\n') {
+    // Vider le buffer avant de lire le nom
+}
 
-    printf("Choisissez %d membres pour l'équipe %s :\n", mode, equipe->nom);
-    afficher_personnages_disponibles(personnages, taille);
+fgets(equipe->nom, MAX_TEAM_NAME, stdin);
+equipe->nom[strcspn(equipe->nom, "\n")] = 0;
+
+printf("Choisissez %d membres pour l'équipe %s :\n", mode, equipe->nom);
+afficher_personnages_disponibles(personnages, taille);
 
     int choix[3];
     for (int i = 0; i < mode; i++) {
@@ -21,10 +24,18 @@ void creer_equipe(equipe *equipe, personnage *personnages, int taille, int mode)
         choix[i] = lire_entier_avec_validation(1, taille);
     }
 
-    equipe->membre1 = personnages[choix[0] - 1];
-    if (mode >= 2) equipe->membre2 = personnages[choix[1] - 1];
-    if (mode == 3) equipe->membre3 = personnages[choix[2] - 1];
-    equipe->pv = pv_equipe(*equipe);
+   equipe->membre1 = personnages[choix[0] - 1];
+
+if (mode >= 2) {
+    equipe->membre2 = personnages[choix[1] - 1];
+}
+
+if (mode == 3) {
+    equipe->membre3 = personnages[choix[2] - 1];
+}
+
+equipe->pv = pv_equipe(*equipe);
+
 }
 
 personnage* vitesse_equipe_membre(equipe *equipe) {
