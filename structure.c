25c46d251bb018043ee9d9938aff348e9af91045
Adamazk <adamarezkii@gmail.com>
2025-05-10 18:24:46 +0200

    Update structure.c

--- structure.c
+++ structure.c
@@ -5,6 +5,8 @@
 #include <unistd.h> // Pour la fonction sleep
 #include <limits.h> // Pour INT_MAX
 
+// Fonction qui calcule les points de vie totaux d'une équipe
 int pv_equipe(equipe equipe1) {
+    // Additionne les PV (points de vie) des trois membres de l’équipe
     return equipe1.membre1.pv + equipe1.membre2.pv + equipe1.membre3.pv;
 }
